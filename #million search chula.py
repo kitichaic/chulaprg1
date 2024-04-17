83875bf7c69d7eb7a3d17f39ce96ef98dfac3b56
Kitichai Chanyalikit (Personal) <127749115+frongfree@users.noreply.github.com>
2024-04-17 15:10:22 +0700

    update

--- #million search chula.py
+++ #million search chula.py
@@ -1,14 +1,16 @@
 #million search chula
 #f = open("million.txt","r")
 f=open("million-sorted.txt","r")
+sites= []
 pos = 0
+name="chula.ac.th"
 for x in f:
     site = x.strip()
-    pos = pos+1
-    if site=="chula.ac.th":
-        break
-        print(x.strip())
-f.close()
+    sites.append(site)
+    
+   
 
-print(pos)
+f.close()
+print (len(sites))
+
 
